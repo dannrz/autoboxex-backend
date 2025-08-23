b01473d268f18ebc66654907120b68707c830006
dannrz <rozd.21d97@gmail.com>
2025-08-22 19:02:03 -0600

    Actualizar patrones de orígenes permitidos en la configuración de CORS para mejorar la seguridad

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['http://localhost:5173', 'http://[::1]:5173', 'http://127.0.0.1:5173'],
 
-    'allowed_origins_patterns' => ['*10.1.5.*'],
+    'allowed_origins_patterns' => ['*localhost*'],
 
     'allowed_headers' => ['*'],
 
