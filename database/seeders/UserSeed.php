3bbfbdb471aca4a4209524f16996e0996687468f
dannrz <rozd.21d97@gmail.com>
2025-09-24 12:43:47 -0600

    Actualizar el rol del usuario 'Alexander Ramirez' en el seeder de usuarios.

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -25,7 +25,7 @@ class UserSeed extends Seeder
             'name' => 'Alexander Ramirez',
             'username' => 'lxndr',
             'password' => Hash::make('asdfG2'),
-            'role_id' => 2,
+            'role_id' => 1,
         ]);
 
         $oldUsers = Usuario::all();
