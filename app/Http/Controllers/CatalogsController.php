a1edc9416564d634635bc1795ad8a9eb086e858e
Daniel <rozd.21d97@gmail.com>
2025-12-04 13:58:43 -0600

    feat: agregar métodos para eliminar y actualizar marcas en el controlador de catálogos

--- app/Http/Controllers/CatalogsController.php
+++ app/Http/Controllers/CatalogsController.php
@@ -51,4 +51,63 @@ class CatalogsController extends Controller
             JsonResponse::HTTP_CREATED
         );
     }
+
+    /**
+     * @api {delete} /brands/{id} Delete brand
+     * @param id brand
+     * @return success message
+     * @throws error message
+     */
+    public function deleteBrand(Request $request): JsonResponse
+    {
+        $validator = Validator::make(['id' => $request->id], [
+            'id' => ['required', 'integer', 'exists:Marca,IdMarca'],
+        ]);
+
+        if ($validator->fails()) {
+            return Response::json(
+                $validator->errors(),
+                JsonResponse::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+        $brand = Brand::find($request->id);
+        $brand->delete();
+
+
+        return Response::json(
+            ['message' => 'Marca eliminada'],
+            JsonResponse::HTTP_OK
+        );
+    }
+
+    /**
+     * @api {put} /brands/{id} Update brand
+     * @param id brand
+     * @return updated brand
+     * @throws error message
+     */
+    public function updateBrand(Request $request): JsonResponse
+    {
+        $validator = Validator::make(array_merge($request->all(), ['id' => $request->id]), [
+            'id' => ['required', 'integer', 'exists:Marca,IdMarca'],
+            'brand' => ['required', 'string', 'max:255', 'unique:Marca,Marca,' . $request->id . ',IdMarca'],
+        ]);
+
+        if ($validator->fails()) {
+            return Response::json(
+                $validator->errors(),
+                JsonResponse::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+
+        $brand = Brand::find($request->id);
+        $brand->Marca = Str::trim($request->brand);
+        $brand->save();
+
+        return Response::json(
+            $brand,
+            JsonResponse::HTTP_OK
+        );
+    }
 }
--- routes/modules/catalogs.php
+++ routes/modules/catalogs.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/brands', '\App\Http\Controllers\CatalogsController@getBrands');
 Route::post('/brands', '\App\Http\Controllers\CatalogsController@saveBrand');
+Route::delete('/brands/{id}', '\App\Http\Controllers\CatalogsController@deleteBrand');
+Route::put('/brands/{id}', '\App\Http\Controllers\CatalogsController@updateBrand');
